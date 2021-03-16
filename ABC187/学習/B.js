ee65a681274a9816969c9806f36a6b68fcb193e6
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-16 10:55:24 +0900

    ABC 187 Merged

---
+++ ABC187/学習/B.js
@@ -0,0 +1,28 @@
+// TODO
+//*   ABC 187 B - Gentle Pairs
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split("\n");
+    const n = parseInt(input[0], 10);
+
+    let count = 0;
+
+    for(let i = 1; i < n; i++) {
+        let [x1, y1] = input[i].split(" ").map(Number);
+        for(let j = i+1; j <= n; j++) {
+            let [x2, y2] = input[j].split(" ").map(Number);
+            let a = (y2 - y1) / (x2 - x1);
+            if (a <= 1 && a >= -1) {
+                count++;
+            }
+        }
+    }
+
+    console.log(count);
+
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
---
+++ ABC187/提出/A_提出.js
@@ -0,0 +1,25 @@
+// TODO
+//*   ABC 187 A - Large Digits
+
+function main(input) {
+    'use strict';
+    let [a, b] = input.split(' ');
+    a = a.split('').map(n => parseInt(n, 10));
+    b = b.split('').map(n => parseInt(n, 10));
+    //* -------------------------------------
+    // console.log(a);
+    // console.log(b);
+    let x = 0;
+    let y = 0;
+
+    for(let i = 0; i < 3; i++) {
+        x += a[i];
+        y += b[i];
+    }
+    const ans = Math.max(x, y);
+
+    console.log(ans);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+// main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
