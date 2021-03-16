418886e318ab70601672084a4ac51fd83317891b
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-16 10:40:36 +0900

    ABC195 changed

--- ABC195/学習/A.js
+++ ABC195/学習/A.js
@@ -1,25 +1,17 @@
 // TODO
-//*   ABC 194 A - I Scream 
+//*   ABC 195 A - Health M Death
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const args = input.split(' ');
-    const a = parseInt(args[0], 10);
-    const b = parseInt(args[1], 10);
-
-    const c = a + b;
-
-    if(c >= 15 && b >= 8) {
-        console.log('1');
-    } else if(c>= 10 && b >= 3) {
-        console.log('2');
-    } else if(c >= 3) {
-        console.log('3');
+    const [M, H] = input.split(' ').map(n => parseInt(n, 10));
+    // console.log([M, H]);
+    //* ------------------------------
+    if(H % M === 0) {
+        console.log('Yes');
     } else {
-        console.log('4');
+        console.log('No');
     }
-
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
--- ABC195/提出/B_提出.js
+++ ABC195/提出/B_提出.js
@@ -1,36 +1,34 @@
 // TODO
-//*   ABC 194 B	Job Assignment
+//*   ABC 106 B - 105
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const args = input.trim().split("\n");
-    const n = parseInt(args[0], 10);
-    //! args.slice(1, N + 1)⇒2番目から要素の最後(N + 1)まで取得
-    const nums = args.slice(1, n + 1);
-    const num = nums.map(n => n.split(" "));
-    const a = num.map(n => parseInt(n[0], 10));
-    const b = num.map(n => parseInt(n[1], 10));
+    let [A, B, W] = input.split(' ').map(Number);
+    // console.log([A, B, W]);
+    //* ------------------------------
+    let m = 1e9;
+    let M = 0;
+    W *= 1000;
 
-    let min = a[0] + b[0];
-    
-    for(let i = 0; i < n; i++) {
-        for(let j = 0; j < n; j++) {
-            let sum_min = 0;
-            //* 同じ配列（＝同じ人間が作業）の場合
-            if(i === j) {
-                // 合計を代入する
-                sum_min = a[i] + b[i];
-            } else { //* 異なる場合
-                //* 大きい方を代入する
-                sum_min = Math.max(a[i],b[j]);
-            }
-            //* 最小の判定（小さい方をanswer）
-            min = Math.min(min, sum_min);
+    //* n=グラム、1gずつ増やす。Wgが上限)
+    for(let n = 1; n <= W; n++) {
+        //* 最小みかん*個数 < 規定のグラム < 最大みかん*個数
+        if(A*n <= W && W <= B*n) {
+            // console.log(`n: ${n}`);
+            // console.log(`A*n: ${A * n}`);
+            // console.log(`B*n: ${B * n}`);
+            m = Math.min(m, n);
+            M = Math.max(M, n);
         }
     }
-    // 結果表示
-    console.log(min);
+
+    if(M !== 0) {
+        console.log(m + ' ' + M);
+    } else {
+        console.log('UNSATISFIABLE');
+    }
+   // console.log('-----------------------');
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
