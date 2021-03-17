919440069715d8f7413bf3e40736e7b94579773d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-17 11:14:32 +0900

    20210317 abc150 A問題

--- ABC150/学習/A.js
+++ ABC150/学習/A.js
@@ -4,8 +4,15 @@
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-
-
+    input = input.split(' ');
+    const k = parseInt(input[0], 10);
+    const x = parseInt(input[1], 10);
+    //* --------------------------------------------
+    if(500*k >= x) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
--- ABC150/提出/A_提出.js
+++ ABC150/提出/A_提出.js
@@ -1,21 +1,18 @@
 // TODO
-//*   ABC 188 A - Three-Point Shot
+//*   ABC 
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const [a, b] = input.split(' ').map(n => parseInt(n, 10));
-    // console.log([a, b]);
-    //* ------------------------------
-    let low = Math.min([a], [b]);
-    let high = Math.max([a], [b]);
-
-    if(low + 3 > high) {
+    input = input.split(' ');
+    const k = parseInt(input[0], 10);
+    const x = parseInt(input[1], 10);
+    //* --------------------------------------------
+    if(500*k >= x) {
         console.log('Yes');
     } else {
         console.log('No');
     }
-
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
