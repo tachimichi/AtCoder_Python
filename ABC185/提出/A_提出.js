ba5ba46a5debb9c88ece780c21496095efaeba35
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-18 23:15:55 +0900

    abc185 @自宅

---
+++ ABC185/提出/A_提出.js
@@ -0,0 +1,15 @@
+// TODO
+//*   ABC 186 A - Brick
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.split(' ');
+    const n = parseInt(input[0], 10);
+    const w = parseInt(input[1], 10);
+    //* ------------------------------
+    console.log(Math.floor(n / w));
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+// main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
