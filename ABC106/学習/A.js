05c983860209404448471651d281e2e3c6d0223a
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-18 15:10:53 +0900

    20210318

--- ABC106/学習/A.js
+++ ABC106/学習/A.js
@@ -1,12 +1,17 @@
 // TODO
-//*   ABC 
+//*   ABC 106 A - Garden
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
+    input = input.split(' ');
+    const a = parseInt(input[0], 10);
+    const b = parseInt(input[1], 10);
+    //* -------------------------------
+    console.log((a-1) * (b-1));
 
 
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
--- ABC106/提出/A_提出.js
+++ ABC106/提出/A_提出.js
@@ -1,21 +1,14 @@
 // TODO
-//*   ABC 188 A - Three-Point Shot
+//*   ABC 106 A - Garden
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const [a, b] = input.split(' ').map(n => parseInt(n, 10));
-    // console.log([a, b]);
-    //* ------------------------------
-    let low = Math.min([a], [b]);
-    let high = Math.max([a], [b]);
-
-    if(low + 3 > high) {
-        console.log('Yes');
-    } else {
-        console.log('No');
-    }
-
+    input = input.split(' ');
+    const a = parseInt(input[0], 10);
+    const b = parseInt(input[1], 10);
+    //* -------------------------------
+    console.log((a-1) * (b-1));
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
