aa2725d42002203a01b00bf5297b7348f7ba2531
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-19 18:00:12 +0900

    b 空白　削除

--- ABC185/学習/B.js
+++ ABC185/学習/B.js
@@ -44,9 +44,6 @@ function main(input) {
     } else {
         console.log("No");
     }
-
-
-
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
