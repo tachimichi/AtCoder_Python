f2bafd9473b2cb978b669d700189de5e78e720bc
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-21 14:04:30 +0900

    1 day 1 problem

--- ABC184/学習/B.js
+++ ABC184/学習/B.js
@@ -8,6 +8,8 @@ function main(input) {
     let [n, x] = input[0].split(' ').map(n => parseInt(n, 10));
     let s = input[1].split('');
     //* ---------------------------------------------------------
+    console.log(s);
+
     for(let i = 0; i < n; i++) {
         if(s[i] === 'o') {
             x++;
@@ -18,5 +20,5 @@ function main(input) {
     console.log(x);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-// main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
--- ABC196/提出/B_提出.js
+++ ABC196/提出/B_提出.js
@@ -5,12 +5,7 @@
 function main(input) {
     'use strict';
     input = input.split('.');
-    let x = parseInt(input[0], 10);
-    let y = Number(input[0]);
     console.log(input[0]);
-    // console.log(x);
-    // console.log(y);
-
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
