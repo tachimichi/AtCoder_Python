ac831db442a0e3227c37967d9be7a1eeffc34e0e
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-22 19:33:40 +0900

    2021/03/22

---
+++ ABC183/学習/B.js
@@ -0,0 +1,11 @@
+// TODO
+//*   ABC 183 B - Billiards
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.split(' ');
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
