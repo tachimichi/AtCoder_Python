c56eeb97972d2f040b636b88c62e9397b47e13ae
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-26 09:03:02 +0900

    0326

--- ABC182/学習/B.js
+++ ABC182/学習/B.js
@@ -1,17 +1,16 @@
 // TODO
-//*   ABC 182 A - twiblr
+//*   ABC 183 B - Billiards
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split(' ');
-    const a = parseInt(input[0], 10);
-    const b = parseInt(input[1], 10);
-    //* ---------------------------------
-    const max = 2 * a + 100;
-    
-    console(max - b);
+    let [x, y, a, b] = input.split(' ').map(n => parseInt(n, 10));
+    const sum = (a - x) / 3;
+
+    let ans = ((x*b) + (a*y)) / (y + b);
+
+    console.log(ans);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
--- ABC182/学習/C_mada.js
+++ ABC182/学習/C_mada.js
@@ -1,17 +1,11 @@
 // TODO
-//*   ABC 182 A - twiblr
+//*   ABC 187 C - 1-SAT
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split(' ');
-    const a = parseInt(input[0], 10);
-    const b = parseInt(input[1], 10);
-    //* ---------------------------------
-    const max = 2 * a + 100;
-    
-    console(max - b);
+
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
