159104ace6cfa2e85dfd363c75803535f0eff46d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-03-27 11:51:53 +0900

    a

---
+++ ABC180/学習/A.js
@@ -0,0 +1,12 @@
+// TODO
+//*   ABC 180 A - box
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const [n, a, b] = input.trim().split(' ').map(n => parseInt(n, 10));
+    console.log(n - a +b);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
