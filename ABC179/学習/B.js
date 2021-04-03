7be1e4268bee96b180be9b2d5e7baa1173764304
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-04 00:42:47 +0900

    d

--- ABC179/学習/B.js
+++ ABC179/学習/B.js
@@ -1,44 +1,24 @@
 // TODO
-//*   ABC 180 B - Various distances
+//*   ABC 179 B - Go to Jail
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('\n');
-    const n = parseInt(input[0], 10);
-    let arr = input[1].split(' ').map(n => parseInt(n, 10));
-    //* ---------------------------------------
-    const ans1 = Manhattan(arr);
-    const ans2 = Euclid(arr);
-    const ans3 = Chebyshev(arr);
-    
-    console.log(ans1);
-    console.log(ans2);
-    console.log(ans3);
-}
-
-function Manhattan(arrayData) {
-    let sum = 0;
-    for(let i = 0; i < arrayData.length; i++) {
-        sum += Math.abs(arrayData[i]);
+    input = input.split('\n');
+    let n = parseInt(input[0], 10);
+    let count = 0;
+    let ans = 'No';
+    //* ---------------------
+    for(let i = 1; i <= n; i ++) {
+        const [x, y] = input[i].split(' ').map(n => parseInt(n, 10));
+        if(x === y) {
+            count += 1;
+        } else {
+            count = 0;
+        }
+        if(count === 3) ans = 'Yes';
     }
-    return sum;
-}
-function Euclid(arrayData) {
-    let sum = 0;
-    for(let i = 0; i < arrayData.length; i++) {
-        sum += Math.pow(arrayData[i], 2);
-    }
-    sum = Math.pow(sum, 1 /2);
-    return sum;
-}
-
-function Chebyshev(arrayData) {
-    let abs = [];
-    for(let i = 0; i < arrayData.length; i++) {
-        abs.push(Math.abs(arrayData[i]));
-    }
-    return Math.max(...abs);
+    console.log(ans);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
