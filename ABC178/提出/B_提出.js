abe9ac7af4e721c857c127b575febb9d2d43f09d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-04 01:23:28 +0900

    aa

--- ABC178/提出/B_提出.js
+++ ABC178/提出/B_提出.js
@@ -1,24 +1,17 @@
 // TODO
-//*   ABC 179 B - Go to Jail
+//*   ABC 178 B - Product Max
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.split('\n');
-    let n = parseInt(input[0], 10);
-    let count = 0;
-    let ans = 'No';
-    //* ---------------------
-    for(let i = 1; i <= n; i ++) {
-        const [x, y] = input[i].split(' ').map(n => parseInt(n, 10));
-        if(x === y) {
-            count += 1;
-        } else {
-            count = 0;
-        }
-        if(count === 3) ans = 'Yes';
-    }
-    console.log(ans);
+    const [a, b, c, d] = input.trim().split(' ').map(n => BigInt(n, 10));
+    //* -----------------
+    let max = a * c;
+    if(a * d > max) max = a * d;
+    if(b * c > max) max = b * c;
+    if(b * d > max) max = b * d;
+    //* -----------------
+    console.log(max.toString());
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 main(require('fs').readFileSync('/dev/stdin', 'utf8'));
--- ABC179/学習/C.js
+++ ABC179/学習/C.js
@@ -4,24 +4,20 @@
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let n = parseInt(input, 10);
-    let array = [];
-    //* ------------------------------
-    for(let i = 1; i <= n / 2; i++) {
-        let x = n / i;
-        if(Number.isInteger(x)) {
-            array.push(x, i);
-        }
+    input = input.trim().split('\n');
+    let n = parseInt(input[0], 10);
+    
+    let sum = 0;
+    for (let a = 1; a <= n; a++) {
+        //TODO a * b + c = nを求める
+        //* a * b = n-c
+        //* aを軸に固定をする(a => iとする)
+        //* c = n - (aの倍数)
+        // n-1以下の整数のうち、aの倍数は何個あるのか
+        sum += Math.floor((n-1) / a);
     }
-    let ans = Array.from(new Set(array));
-    ans.sort(
-        function(a,b) {
-            return a - b;
-        }
-    )
-    ans.forEach(element => {
-        console.log(element);
-    });
+    
+    console.log(sum);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
