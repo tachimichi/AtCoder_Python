09f840cc9d1ecf834abcdd3ec1905b2d125795ac
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-04 00:47:10 +0900

    a

---
+++ ABC178/学習/A.js
@@ -0,0 +1,16 @@
+// TODO
+//   ABC 178 A - Not
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const n = parseInt(input, 10);
+    if(n === 1) {
+        console.log(0);
+    } else {
+        console.log(1);
+    }
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+// main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
---
+++ ABC178/提出/B_提出.js
@@ -0,0 +1,25 @@
+// TODO
+//*   ABC 179 B - Go to Jail
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.split('\n');
+    let n = parseInt(input[0], 10);
+    let count = 0;
+    let ans = 'No';
+    //* ---------------------
+    for(let i = 1; i <= n; i ++) {
+        const [x, y] = input[i].split(' ').map(n => parseInt(n, 10));
+        if(x === y) {
+            count += 1;
+        } else {
+            count = 0;
+        }
+        if(count === 3) ans = 'Yes';
+    }
+    console.log(ans);
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+// main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
