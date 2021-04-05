341df9563619461ded437a5b4788f2b80b473dc8
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-05 17:28:43 +0900

    asdfss

--- ABC185/学習/C.js
+++ ABC185/学習/C.js
@@ -19,5 +19,5 @@ function main(input) {
     
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-// main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
