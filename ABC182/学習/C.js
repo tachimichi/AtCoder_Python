e2a0115d6b353bec0261b956ad1097c2b8b6bba0
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-05 16:27:22 +0900

    aasa

--- ABC182/学習/C.js
+++ ABC182/学習/C.js
@@ -7,23 +7,38 @@ function main(input) {
     let n = parseInt(input, 10);
     let f = input.split('').map(n => parseInt(n, 10));
     //* ----------------------------
-    console.log(n);
-    console.log(f);
-    const x = n % 3;
-    console.log(x);
+    let single = 0;
+    let double = 0;
+    let d = 0;
+    let sum = 0;
+    let keta = 0;
 
-    let s = 0;
-    let k = 0;
     while(n) {
-        let d = n % 10;
-        
+        d = n % 10;
+        if(d % 3 === 1) single++;
+        if(d % 3 === 2) double++;
+        n = Math.floor(n / 10);
+        sum += d;
+        keta++;
     }
 
-    if(x === 0) return console.log(0);
-    else if (x === 1) {
+    // console.log(single, double);
+    // console.log(sum);
+    // console.log(keta);
 
-    }
+    if(sum % 3 === 0) return console.log(0);
+    if(n.length === 1) return console.log(-1);
     
+    if (sum % 3=== 1) {
+        if(single >= 1 && keta > 1) return console.log(1);
+        if(double >= 2 && keta > 2) return console.log(2);
+    }
+    else if(sum % 3 === 2) {
+        if(double >= 1 && keta > 1) return console.log(1);
+        if(single >= 2 && keta > 2) return console.log(2);
+    }
+    console.log(-1);
+
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // main(require('fs').readFileSync('/dev/stdin', 'utf8'));
--- ABC182/提出/C_提出.js
+++ ABC182/提出/C_提出.js
@@ -1,16 +1,43 @@
 // TODO
-//*   ABC 186 A - Brick
+//*   ABC 182 C - To 3
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.split(' ');
-    const n = parseInt(input[0], 10);
-    const w = parseInt(input[1], 10);
-    //* ------------------------------
-    console.log(Math.floor(n / w));
-    
+    let n = parseInt(input, 10);
+    let f = input.split('').map(n => parseInt(n, 10));
+    //* ----------------------------
+    let single = 0;
+    let double = 0;
+    let d = 0;
+    let sum = 0;
+    let keta = 0;
 
+    while(n) {
+        d = n % 10;
+        if(d % 3 === 1) single++;
+        if(d % 3 === 2) double++;
+        n = Math.floor(n / 10);
+        sum += d;
+        keta++;
+    }
+
+    // console.log(single, double);
+    // console.log(sum);
+    // console.log(keta);
+
+    if(sum % 3 === 0) return console.log(0);
+    if(n.length === 1) return console.log(-1);
+    
+    if (sum % 3=== 1) {
+        if(single >= 1 && keta > 1) return console.log(1);
+        if(double >= 2 && keta > 2) return console.log(2);
+    }
+    else if(sum % 3 === 2) {
+        if(double >= 1 && keta > 1) return console.log(1);
+        if(single >= 2 && keta > 2) return console.log(2);
+    }
+    console.log(-1);
 
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
