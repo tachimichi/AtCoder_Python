f72b473c1bb608fa904db55b0706d83e9c4de428
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-06 17:05:10 +0900

    c

--- ABC178/学習/C.js
+++ ABC178/学習/C.js
@@ -1,28 +1,35 @@
 // TODO
-//*   ABC 180 C - Cream puf
+//*   ABC 178 C - Ubiquity
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    let n = parseInt(input, 10);
-    let array = [];
-    //* ------------------------------
-    for(let i = 1; i <= n / 2; i++) {
-        let x = n / i;
-        if(Number.isInteger(x)) {
-            array.push(x, i);
-        }
+    const int = parseInt(input, 10);
+    console.log(int);
+    const n = BigInt(input.trim());
+    console.log(n);
+    //* ----------------------------
+    let a = 10;
+    let b = 9;
+    let c = 8;
+    let mod = 1e9+7;
+    //* -----------------------------
+    for (let i = 1; i < n; i++) {
+        a = (10 * a) % mod;
+        b = (9 * b) % mod;
+        c = (8 * c) % mod;
+        console.log(a, b, b, c);
     }
-    let ans = Array.from(new Set(array));
-    ans.sort(
-        function(a,b) {
-            return a - b;
-        }
-    )
-    ans.forEach(element => {
-        console.log(element);
-    });
+    let ans = (a - 2 * b + c) % mod;
+    console.log(a - 2 * b + c);
+    console.log(ans);
+    //* 
+    console.log((ans + mod) % mod);
+    
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-// main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+}
