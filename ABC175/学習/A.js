fb4b2a889fee9d14ba73f280492020bfd939e2cb
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-07 14:24:07 +0900

    難しい

---
+++ ABC175/学習/A.js
@@ -0,0 +1,29 @@
+// TODO
+//*   ABC 175 A - Rainy Season
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    //* --------------------------------
+    const s = input.trim().split('');
+    //----------------------------------
+    const p = s[0] == 'R';
+    const q = s[1] == 'R';
+    const r = s[2] == 'R';
+
+    if(p && q && r) {
+        console.log(3);
+    } else if((p && q) || (q && r)) {
+        console.log(2);
+    } else if(p || q || r) {
+        console.log(1);
+    } else {
+        console.log(0);
+    }
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+}
---
+++ ABC175/学習/B.js
@@ -0,0 +1,27 @@
+// TODO
+//*   ABC 176 B - Multiple of 9
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const n = input;
+    //* --------------------------
+    // console.log(n);
+    // console.log(n.length);
+    
+    let sum = 0n;
+    for(let i = 0; i < n.length; i++) {
+        sum += BigInt(input[i], 10);
+    }
+    if(sum % 9n === 0n) {
+        console.log('Yes');
+    } else { 
+        console.log('No');
+    }
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+}
