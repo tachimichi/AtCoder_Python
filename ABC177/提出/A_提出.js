11dcddd1c010b98e117c588b0ca398a6daff4d7e
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-07 09:05:15 +0900

    568

---
+++ ABC177/提出/A_提出.js
@@ -0,0 +1,21 @@
+// TODO
+//*   ABC 177 A - Don't be late
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const [d, t, s] = input.split(' ').map(n => parseInt(n, 10));
+    //* --------------------------------
+    if(s * t >= d) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+}
---
+++ ABC177/提出/C_提出.js
@@ -0,0 +1,31 @@
+// TODO
+//*   ABC 178 C - Ubiquity
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const n = BigInt(input.trim());
+    //* ----------------------------
+    let a = 10;
+    let b = 9;
+    let c = 8;
+    let mod = 1e9+7;
+    //* -----------------------------
+    for (let i = 1; i < n; i++) {
+        a = (10 * a) % mod;
+        b = (9 * b) % mod;
+        c = (8 * c) % mod;
+        // console.log(a, b, b, c);
+    }
+    let ans = (a - 2 * b + c) % mod;
+    //* 
+    console.log((ans + mod) % mod);
+    
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+}
