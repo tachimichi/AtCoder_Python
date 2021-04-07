102135cb262c48e4cb14bc5ef1eda979f1acee6d
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-07 13:11:52 +0900

    b

--- ABC176/提出/B_提出.js
+++ ABC176/提出/B_提出.js
@@ -1,24 +1,23 @@
 // TODO
-//*   ABC 177 B - Substring
+//*   ABC 176 B - Multiple of 9
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const [s, t] = input.trim().split('\n');
-    //* -----------------
-    let ans = t.length;
-    //* ---------------------
-    //* sの何文字目から調べるかを全探索
-    for(let start = 0; start <= s.length - t.length; start++) {
-        let diff = 0;
-        //* 文字列tのi番目と、s[ループn回目+i番目]が一致するか確認
-        for(let i = 0; i < t.length; i++) {
-            if(t[i] != s[start + i]) diff++;
-        }
-        ans = Math.min(ans, diff);
+    const n = input;
+    //* --------------------------
+    // console.log(n);
+    // console.log(n.length);
+    
+    let sum = 0n;
+    for(let i = 0; i < n.length; i++) {
+        sum += BigInt(input[i], 10);
     }
-    console.log(ans);
-
+    if(sum % 9n === 0n) {
+        console.log('Yes');
+    } else { 
+        console.log('No');
+    }
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
