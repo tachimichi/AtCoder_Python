2e4393df147538804dd761acca1ddab3d96e0605
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-08 16:04:17 +0900

    172

---
+++ 151 ~ 175/ABC172/提出/A_提出.js
@@ -0,0 +1,16 @@
+// TODO
+//*   ABC 172 A - Calc
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const a = parseInt(input, 10);
+    console.log(a + a**2 + a**3);
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+}
