c64cd5ba8cd545f15944ef4dda1f5e8af253c4a6
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-08 12:02:45 +0900

    bit

--- 151 ~ 175/ABC173/学習/C.js
+++ 151 ~ 175/ABC173/学習/C.js
@@ -1,32 +1,37 @@
 // TODO
-//*   ABC 177 C - Walking Takahashi
+//*   ABC 173 C - H and V
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    let [x, k, d] = input[0].split(' ').map(n => parseInt(n, 10));
-    //* ----------------------------
-    // 負でも「座標の絶対値」なので、abs
-    x = Math.abs(x);
-
-    //* xをd（移動距離）で割った数（最低でもまっすぐに進む回数）
-    let staright = Math.floor(x/d);
-    //* xをd（移動距離）で割ったあまり
-    let r = x % d;
-
-    //* k回では近づくことが無理な場合
-    if(staright >= k) return console.log(x - d*k);
-    //* 余分に移動する回数
-    let urouro = k - staright;
-
-    if(urouro % 2 === 0) {
-        //* 偶数なら、現在地
-        console.log(r);
-    } else {
-        //* 奇数なら、一回分の距離を考慮
-        console.log(Math.abs(r - d));
+    const [h, w, k] = input[0].split(' ').map(n => parseInt(n, 10));
+    const n = [];
+    for (let i = 1; i <= h; i++) {
+        let t = input[i].split('');
+        n.push(t);
     }
+    //* -------------------------------
+    //? 前提：h行目を塗るのか、w列目を塗るのか
+    //? bit全探索：2^h+w通り。
+    //? 制約：h, w <6(4096通り)
+
+    let ans = 0;
+
+    for(let yoko = 0; yoko < (1 << h); yoko++) {
+        for(let tate = 0; tate < (1 << w); tate++) {
+            let black = 0;
+            for(let i =0; i < h; i++) {
+                for(let j =0; j < w; j++) {
+                    if(((yoko >> i) & 1) == 0 
+                        && ((tate >> j) & 1) == 0
+                        && n[i][j] == '#' ) black++;
+                }
+            }
+            if(black == k) ans++;
+        }
+    }
+    console.log(ans);
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
