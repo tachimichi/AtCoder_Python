56f0a1a03bf475bfb4317d824048cc8dd3ca07f6
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-10 12:00:38 +0900

    1問

---
+++ 151 ~ 175/ABC167/学習/A.js
@@ -0,0 +1,24 @@
+// TODO
+//*   ABC 167 A - Registration 
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const s = input[0].split('');
+    const t = input[1].split('');
+    //* ----------------------
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] !== t[i]) {
+            return console.log('No');
+        }
+    }
+    console.log('Yes');
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error) {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+}
