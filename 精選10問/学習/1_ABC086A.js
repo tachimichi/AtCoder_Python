8cb1672862236942c820b62f1e8b330707be1cd1
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-12 15:24:40 +0900

    AtCoderへのtry-catchのerror表示対応④

--- 精選10問/学習/1_ABC086A.js
+++ 精選10問/学習/1_ABC086A.js
@@ -24,7 +24,13 @@ function main(input) {
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // '/dev/stdin'に判定用のデータが格納されていると考えられる。
 try {
-    main(require('fs').readFileSync('../txt/1_ABC086A.txt', 'utf8'));
-} catch (error) {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/1_ABC086A.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
 }
--- 精選10問/学習/5_ABC083B.js
+++ 精選10問/学習/5_ABC083B.js
@@ -61,7 +61,13 @@ function getSumOfDigits(value) {
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 // '/dev/stdin'に判定用のデータが格納されていると考えられる。
 try {
-    main(require('fs').readFileSync('../txt/5_ABC083B.txt', 'utf8'));
-} catch (error) {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+        main(require('fs').readFileSync('../txt/5_ABC083B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
 }
