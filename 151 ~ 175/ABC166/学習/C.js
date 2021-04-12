29553966df2f599fa7f1dfa4450ffabfe166de11
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-12 15:04:16 +0900

    c

--- 151 ~ 175/ABC166/学習/C.js
+++ 151 ~ 175/ABC166/学習/C.js
@@ -1,71 +1,49 @@
 // TODO
-//*   ABC 167 C - Skill Up
-
+//*   ABC 166 C - Peaks
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const [n, m, x] = input[0].trim().split(' ').map(n => parseInt(n, 10)); // 1行目の取得
-    input.shift(); //* 1行目の削除（破壊的メソッド）
-    //* 行に対するmap処理と、要素に対するmap処理
-    let books = input.map(element => element.split(' ').map(n => parseInt(n, 10)));
-    console.log(`n, m, x`, n, m, x);
-    console.log(books);
-    //* --------------------------------
-    //? 前提：n冊の参考書を買うor買わない「2^n(12) => 4096通り」
-    //? 達成条件：M個のアルゴリズムの理解度がx以上 && 金額が最小値
-    //? --------------------------------------------------------------
-    //? 2進数の論理積：(i>>j) & 1)
-    //? iをj回、右にシフトさせる（割る2的な）、右端にある数値が(2進数で)1かどうか判断する
-    //? 買う場合を1、買わない場合を0と考えて、スキルの最小値がx以上かどうか判定する
-    //? x以上の場合、リストに追加していく
-    //? リストを昇順に並び替えると、最小値が判明する（空のリストの場合は、-1）
-    //* --------------------------------
-    let sum;
-    let min = 1e+10;
-    for(let i = 0; i < 2**n; i ++) {
-        //* 配列の生成
-        let t = new Array(m+1).fill(0);
-        sum = 0;
-        console.log(`t:`);
-        console.log(t);
-        console.log(`*********************`);
-        // ----------------------
-        for(let j = 0; j < n; j++) {
-            // jを1回、左シフトする && 論理積が1
-            //* 買う選択した場合（bit全探索）
-            if(i & (1 << j)) {
-                for(let k = 0; k <= m; k++) {
-                    t[k] += books[j][k];
-                    console.log(t);
-                    console.log(`-------------------`);
-                }
-            }
-        }
+    const [n, m] = input[0].split(' ').map(n => parseInt(n, 10));
+    const h = input[1].split(' ').map(n => parseInt(n, 10));
+    // -------------------------------
+    let a = [];
+    let b = [];
+    for(let i = 2; i < input.length; i++) {
+        const [t1, t2] = input[i].split(' ').map(n => parseInt(n, 10));
+        a.push(t1);
+        b.push(t2);
+    }
+    console.log(a, b);
+    //* ------------------------------
+    //* 他の山より大きい場合のみ、1とする（bit全探索風）
+    let t = new Array(n).fill(1);
+    console.log(t);
+    for(let i = 0; i<m; i++) {
+        //* 線でつながる箇所同士を比較する
+        //* (小さい方or同値)を探して、0にする
+        //? 残った要素が良い山として条件に当てはまる
+        if(h[a[i]-1] >= h[b[i]-1]) t[b[i]-1] = 0;
+        if(h[a[i]-1] <= h[b[i]-1]) t[a[i]-1] = 0;
+    }
+    console.log(t);
 
-        //* i番目の探索した結果について
-        let isBig = true;
-        for(let l = 1; l <= m; l++) {
-            // t[l]番目の能力がx以上あるかどうか
-            if(t[l] < x) {
-                isBig = false;
-                break;
-            }
-        }
-        //* 個々の能力がm以上ある場合、金額が最小か判定する
-        if(isBig) min = Math.min(t[0], min);
+    let ans = 0;
+    for(let i = 0; i<t.length;i++) {
+        if(t[i] == 1) ans++;
     }
-    // もし最小が1e+10の場合は、-1
-    // 否定の場合、minを出力する
-    if(min != 1e+10){
-        console.log(min);
-    } else {
-        console.log(-1);
-    }
+    console.log('ans', ans);
+
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
-    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
-} catch (error) {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
 }
--- 151 ~ 175/ABC166/提出/C_提出.js
+++ 151 ~ 175/ABC166/提出/C_提出.js
@@ -1,36 +1,46 @@
 // TODO
-//*   ABC 166 B - Trick or Treat
-
+//*   ABC 166 C - Peaks
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
-    const [n, k] = input[0].split(' ').map(n => parseInt(n, 10));
-    //* -------------------------------
-    //? i行目の配列ごとにmapの中で、pushを行うと
-    //? aの中身が、1つの配列で管理できる
+    const [n, m] = input[0].split(' ').map(n => parseInt(n, 10));
+    const h = input[1].split(' ').map(n => parseInt(n, 10));
+    // -------------------------------
     let a = [];
-    for(let i = 2; i < input.length; i += 2) {
-        input[i].split(' ').map(n => a.push(parseInt(n, 10)));
+    let b = [];
+    for(let i = 2; i < input.length; i++) {
+        const [t1, t2] = input[i].split(' ').map(n => parseInt(n, 10));
+        a.push(t1);
+        b.push(t2);
     }
-    //* -------------------------------
-    let d = Array(n).fill(0);
-    for(let i = 0; i < a.length; i++) {
-        // a[i]で該当するn番目のスヌケを、d=1としてbit風に色付けする
-        d[a[i]-1] = 1;
+    //* ------------------------------
+    //* 他の山より大きい場合のみ、1とする（bit全探索風）
+    let t = new Array(n).fill(1);
+    for(let i = 0; i<m; i++) {
+        //* 線でつながる箇所同士を比較する
+        //* (小さい方or同値)を探して、0にする
+        //? 残った要素が良い山として条件に当てはまる
+        if(h[a[i]-1] >= h[b[i]-1]) t[b[i]-1] = 0;
+        if(h[a[i]-1] <= h[b[i]-1]) t[a[i]-1] = 0;
     }
 
-    //* 0の箇所をカウントする
-    let count = 0;
-    for(let i = 0; i < d.length; i++) {
-        if(d[i] === 0) count++;
+    let ans = 0;
+    for(let i = 0; i<t.length; i++) {
+        if(t[i] == 1) ans++;
     }
-    console.log(count);
-
+    console.log(ans);
 }
+
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
-    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
-} catch (error) {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
 }
