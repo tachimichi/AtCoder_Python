dde518f90a79c75a4f5c21ce700192a9f4ef6938
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-12 16:14:07 +0900

    ABC165 Cは未実施

---
+++ 151 ~ 175/ABC165/学習/B.js
@@ -0,0 +1,35 @@
+// TODO
+//*   ABC 165 B - 1%
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const x = BigInt(input.trim());
+    // console.log(x);
+    // --------------------------
+    //? 注意：10^18等は、桁数注意 
+    let count = 0;
+    let t = 100n;
+    //* 福利の計算
+    while(t < x) { // tがxに達していない時
+        //* BigIntにmath.floorは使用できない
+        t = t * 101n / 100n; 
+        // t += Math.floor(t / 100);
+        count++;
+    }
+    console.log(count);
+}
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
---
+++ 151 ~ 175/ABC165/提出/A_提出.js
@@ -0,0 +1,28 @@
+// TODO
+//*   ABC 165 A - We Love Golf
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const k = parseInt(input[0], 10);
+    const [a, b] = input[1].split(' ').map(n => parseInt(n, 10));
+    // --------------------------------------
+    for(let i = a; i <= b; i++) {
+        if(i % k === 0) return console.log('OK');
+    }
+    console.log('NG');
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
