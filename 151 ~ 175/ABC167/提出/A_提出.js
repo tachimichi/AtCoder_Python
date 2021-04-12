56beed1a3d8204ae4933d0ef0b8735f0138663c2
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-12 15:09:56 +0900

    AtCoderへのtry-catchのerror表示対応

--- 151 ~ 175/ABC167/提出/A_提出.js
+++ 151 ~ 175/ABC167/提出/A_提出.js
@@ -31,7 +31,13 @@ function main(input) {
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
-    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
-} catch (error) {
     main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
 }
