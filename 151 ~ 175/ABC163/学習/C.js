e6ad94c1f8d4a04a6ef170bfd4a54a296d25f971
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-13 16:35:13 +0900

    ABC163

--- 151 ~ 175/ABC163/学習/C.js
+++ 151 ~ 175/ABC163/学習/C.js
@@ -1,21 +1,25 @@
 // TODO
-//*   ABC 164 C - gacha
+//*   ABC 163 C - management
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     input = input.trim().split('\n');
     const n = parseInt(input[0], 10);
-    let str = [];
-    for(let i = 1; i < input.length; i++) {
-        const hoge = input[i].trim();
-        str.push(hoge);
+    const a = input[1].split(' ').map(n => parseInt(n, 10));
+    // console.log(a);
+    //* -----------------------------------
+    // 要素n個の配列を作成
+    let t = new Array(n).fill(0);
+    //* a[i]番目をカウントする⇒部課の数をカウントする
+    for(let i = 0; i < n-1; i++) {
+        t[a[i]-1]++;
     }
-    // console.log(str);
-    
-    str = Array.from(new Set(str));
-    // console.log(str);
-    console.log(str.length);
-    
+    // t.forEach(element => {
+    //     console.log(element);
+    // });
+    for(let i = 0; i < t.length; i++) {
+        console.log(t[i]);
+    }
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
