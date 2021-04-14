9be77dd9bb607bc0fba9ca4b30e558c3c002dae3
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-14 10:45:14 +0900

    abc161

---
+++ 151 ~ 175/ABC161/学習/A.js
@@ -0,0 +1,26 @@
+// TODO
+//*   ABC 161 A - ABC Swap
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const [x, y, z] = input.trim().split(' ').map(n => parseInt(n, 10));
+    // ---------------------
+    console.log(z, x, y);
+
+}
+
+
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
---
+++ 151 ~ 175/ABC161/学習/B.js
@@ -0,0 +1,40 @@
+// TODO
+//*   ABC 161 B - Popular Vote
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const [n, m] = input[0].split(' ').map(n =>parseInt(n, 10));
+    const a = input[1].split(' ').map(n =>parseInt(n, 10));
+    // -----------------------
+    // console.log(a);
+    a.sort((a,b) => b - a);
+    // console.log(a);
+    
+    let sum = 0;
+    for(let i = 0; i < n; i++) {
+        sum += a[i];
+    }
+
+    if(a[m-1] >= sum/(4*m)) {
+        console.log('Yes');
+    } else {
+        console.log('No');
+    }
+
+}
+
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
