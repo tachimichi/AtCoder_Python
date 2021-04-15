4268e2bca3b7579ccb35829858a3b06a0cbce104
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-15 15:34:43 +0900

    abc159

--- 151 ~ 175/ABC159/学習/B.js
+++ 151 ~ 175/ABC159/学習/B.js
@@ -1,18 +1,32 @@
 // TODO
-//*   ABC 160 B - Golden Coins
+//*   ABC 159 B - String Palindrome
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    const x = parseInt(input, 0);
+    const s = input.trim();
+    const n = s.length;
     // ------------------------
-    let t = x % 500;
-    let sum = Math.floor(x/500)*1000 + Math.floor(t/5)*5;
-    console.log(sum);
-
-
+    // console.log(s);
+    // console.log(check(s));
+    //* -----------------------
+    if(check(s) && check(s.slice(0, Math.floor(s.length / 2))) && check(s.slice(Math.ceil(s.length / 2))) ) {
+        return console.log('Yes');
+    }
+    console.log('No');
 }
 
+function reverseStr(s) {
+    let rs = "";
+    for (let i = 0, n = s.length; i < n; i++) {
+        rs += s[n-i-1];
+    }
+    return rs;
+}
+
+function check(str) {
+    return str === reverseStr(str);
+}
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
