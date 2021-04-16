d5fa4bc09ce0f8cb177197043da74b921baed668
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-16 21:37:12 +0900

    C問題 理解していない

--- 151 ~ 175/ABC159/提出/C_提出.js
+++ 151 ~ 175/ABC159/提出/C_提出.js
@@ -1,26 +1,11 @@
 // TODO
-//*   ABC 160 C - Traveling Salesman around Lake
+//*   ABC 159 C - Maximum Volume
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
-    input = input.trim().split('\n');
-    const [k, n]  = input[0].trim().split(' ').map(n => parseInt(n, 10));
-    const a = input[1].split(' ').map(n => parseInt(n, 10));
+    const l = parseInt(input, 10);
     // -----------------------------
-    //* 初期値の設定(「スタート地点と1番目の距離」)
-    let maxDistance = a[0];
-    console.log('maxDistance', maxDistance);
-
-    //* 「aとa-1番目の距離」と、maxDistanceの比較
-    for(let i = 1; i < a.length; i++) {
-        maxDistance = Math.max(maxDistance, a[i]-a[i-1]);
-    }
-    //* 「1番目とn番目の距離（絶対値）」と、maxDistanceの比較
-    console.log('maxDistance', maxDistance);
-    maxDistance = Math.max(maxDistance, Math.abs(k-a[n-1]+a[0]));
-    console.log('maxDistance', maxDistance);
-
-    console.log(k -maxDistance);
+    console.log((l/3)*(l/3)*(l/3));
 }
 
 
