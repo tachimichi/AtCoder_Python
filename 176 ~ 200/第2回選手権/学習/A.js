fdd1881f34c98b0202ffe678612686d380fe5c53
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-22 14:58:46 +0900

    A問題　復習

--- 176 ~ 200/第2回選手権/学習/A.js
+++ 176 ~ 200/第2回選手権/学習/A.js
@@ -1,19 +1,12 @@
 // TODO
 //*   第二回日本最強プログラマー学生選手権 
 
-
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict';
     const [x, y, z] = input.split(' ').map(n =>parseInt(n, 10));
     //* ------------------------
-    const a = y/x;
-    const b = z * a;
-    if(y == b) {
-        console.log(Math.floor(b)-1);
-    } else {
-        console.log(Math.floor(b));
-    }
+    console.log(Math.floor((y*z-1)/x));
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
