10ef2dea8b8dd0a9ace2b775614b807de6a2095b
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-22 14:32:26 +0900

    B問題の復習

--- 176 ~ 200/ABC198/提出/B_提出.js
+++ 176 ~ 200/ABC198/提出/B_提出.js
@@ -1,47 +1,23 @@
 // TODO
 //*   ABC 198 B - Palindrome with leading zeros
 
-// 関数mainに標準入力をinputとして受け取る
 function main(input) {
-    'use strict';
-    //* 数字の分割
-    let n = input.trim().split('').map(n => parseInt(n, 10));
-    console.log(`n:`);
-    console.log(n);
+    //* 変数の初期化
+    N = input.trim();
     //* ---------------
-    //* 反転させる関数を使用する
-    //? arr.reverse()だと、破壊的な性質のため、元の配列も変更される
-    let n2 = reverse(n);
-    console.log(`n2`);
-    console.log(n2);
-    console.log(`n:`);
-    console.log(n);
-    //* 元から回文かどうか判定する()
-    //* toString無しだと、配列自体の比較となるため中身が一致してもダメ
-    if(n.toString() == n2.toString()) return console.log('Yes');
+    // console.log(N);
+    //* 条件：Nは最大10桁なので、i<10とする
+    let i = 0
+    while(i < 10) {
+        //* 文字列Nが分割して反転しても、Nと同じなら
+        if(N == N.split('').reverse().join('')) return console.log('Yes');
 
-    //* -----------------
-    for(let i = 0; i < n.length; i++) {
-        // 末尾に0を挿入
-        n2.push(0);
-        console.log(`n2+0`);
-        console.log(n2);
-        // 反転させる
-        const str = reverse(n2);
-        console.log(`str:`);
-        console.log(str);
-        // 末尾に0を足したものが回文か判定する
-        if(str.toString() === n2.toString()) return console.log('Yes');
-        console.log(`--------------------`);
+        //* 次のループ処理のためのステップ式
+        N = '0' + N;
+        i += 1;
+        // console.log(N);
     }
-    console.log('No');
-}
-
-function reverse(arr){
-    if(toString.call(arr) !== '[object Array]') return null;
-    if(arr.length === 0) return arr;
-    let copy = arr.slice();
-    return copy.reverse();
+    console.log('No')
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
 try {
