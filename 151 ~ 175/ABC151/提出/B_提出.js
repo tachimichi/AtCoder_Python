af897788b3fc50ea141159ce95183410206f96f5
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-04-23 10:19:36 +0900

    ABC151

---
+++ 151 ~ 175/ABC151/提出/B_提出.js
@@ -0,0 +1,40 @@
+// TODO
+//*   ABC 151 B - Achieve the Goal 
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    input = input.trim().split('\n');
+    const [n, k, m] = input[0].split(' ').map(n => parseInt(n, 10));
+    const a = input[1].split(' ').map(n => parseInt(n, 10));
+    //* --------------------------
+    let sum = 0;
+    for(let i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+
+    let t = n*m - sum;
+    if(t<=0) { 
+        console.log(0);
+    } else {
+        if(t > k) {
+            console.log(-1);
+        } else {
+            console.log(t);
+        }
+    }
+
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
