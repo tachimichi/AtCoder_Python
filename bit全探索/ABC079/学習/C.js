5c29cb7bb54b6a00416c6ed750671152c40fa505
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-03 12:51:49 +0900

    C.jsのテキスト読み込み変更

--- bit全探索/ABC079/学習/C.js
+++ bit全探索/ABC079/学習/C.js
@@ -27,14 +27,4 @@ function main(input) {
     }
 }
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
-try {
-    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
-} catch (error1) {
-    try {
-    main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
-    } catch (error2) {
-        console.log('error2', error2);
-        console.log('----------------------------');
-        console.log('error1', error1);
-    }
-}
+main(require('fs').readFileSync('../txt/C.txt', 'utf8'));
