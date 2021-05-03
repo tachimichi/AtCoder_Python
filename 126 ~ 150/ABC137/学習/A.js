eca2c6d346611f321d662875ce51e821a6b3a769
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-03 12:27:48 +0900

    ABC137

---
+++ 126 ~ 150/ABC137/学習/A.js
@@ -0,0 +1,27 @@
+// TODO
+//*   ABC 147 A - Blackjack
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict';
+    const a = input.trim().split(' ').map(n => parseInt(n, 10));
+    let sum = a[0]+a[1]+a[2];
+    if(sum>21) {
+        console.log('bust');
+    } else {
+        console.log('win');
+    }
+
+}
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/A.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
---
+++ 126 ~ 150/ABC137/学習/B.js
@@ -0,0 +1,32 @@
+// TODO
+//*   ABC 147 B - Palindrome-philia
+
+// 関数mainに標準入力をinputとして受け取る
+function main(input) {
+    'use strict'
+    let str = input.trim().split('');
+    // console.log(str);
+    
+    
+    let count = 0;
+    for(let i = 0; i < str.length; i++) {
+        if(str[i] != str[str.length - i-1]) count++;
+        // console.log(str[i]);
+        // console.log(str[str.length - i-1]);
+    }
+    console.log(count/2);
+    
+}
+
+//*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
+try {
+    main(require('fs').readFileSync('/dev/stdin', 'utf8'));
+} catch (error1) {
+    try {
+    main(require('fs').readFileSync('../txt/B.txt', 'utf8'));
+    } catch (error2) {
+        console.log('error2', error2);
+        console.log('----------------------------');
+        console.log('error1', error1);
+    }
+}
