dd144d4fe58040a60f58785ce41c1ab1fbe61e25
tachimichi <72062383+tachimichi@users.noreply.github.com>
2021-05-04 17:17:27 +0900

    B問題

--- 126 ~ 150/ABC146/学習/B.js
+++ 126 ~ 150/ABC146/学習/B.js
@@ -1,21 +1,24 @@
 // TODO
-//*   ABC 147 B - Palindrome-philia
+//*   ABC 146 B - ROT N
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
     'use strict'
-    let str = input.trim().split('');
-    // console.log(str);
-    
-    
-    let count = 0;
-    for(let i = 0; i < str.length; i++) {
-        if(str[i] != str[str.length - i-1]) count++;
-        // console.log(str[i]);
-        // console.log(str[str.length - i-1]);
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    let str = input[1].split('');
+    let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    //* -----------
+
+    let ans = "";
+    for (let i = 0; i < str.length; i++) {
+        let t = str[i];
+        for (let j = 0; j < abc.length/2; j++) {
+            if(t == abc[j]) ans += abc[j+n];
+        }
     }
-    console.log(count/2);
-    
+    console.log(ans);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
--- 126 ~ 150/ABC146/提出/B_提出.js
+++ 126 ~ 150/ABC146/提出/B_提出.js
@@ -1,18 +1,24 @@
 // TODO
-//*   ABC 149 B - Greedy Takahashi
+//*   ABC 146 B - ROT N
 
 // 関数mainに標準入力をinputとして受け取る
 function main(input) {
-    'use strict';
-    let [a, b, k] = input.trim().split(' ').map(n => parseInt(n, 10));
+    'use strict'
+    input = input.trim().split('\n');
+    const n = parseInt(input[0], 10);
+    let str = input[1].split('');
+    let abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    if(a+b <= k) {
-        console.log(0, 0);
-    } else if(a >= k) {
-        console.log(a-k, b);
-    } else {
-        console.log(0, a+b-k);
+    //* -----------
+
+    let ans = "";
+    for (let i = 0; i < str.length; i++) {
+        let t = str[i];
+        for (let j = 0; j < abc.length/2; j++) {
+            if(t == abc[j]) ans += abc[j+n];
+        }
     }
+    console.log(ans);
 }
 
 //*この行以降は編集しないでください（標準入出力から一度に読み込み、Mainを呼び出します）
