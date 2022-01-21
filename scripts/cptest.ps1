291327169cbb7f0166e6cceccec3cf9fce2b92d3
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-21 17:14:43 +0900

    改修

--- scripts/cptest.ps1
+++ scripts/cptest.ps1
@@ -1,23 +1,23 @@
 # 解答中のファイル名
 $problem_name = $Args[0]
-# src\コンテスト階層名
+# src\コンテスト階層名\カレントのコンテスト名
 $fileDirname = $Args[1]
-# src / コンテスト階層名
-$folderDirname,$folderDirname2 = ($fileDirname -split "\\")
+# src / コンテスト階層名 / カレントのコンテスト名
+$src,$folderBitween,$folderCurrent = ($fileDirname -split "\\")
 # ABCnnn / (a,b,c)
-$problem_name_list = ($problem_name -split "_")
+# $problem_name_list = ($problem_name -split "_")
 # ABCnnn
 $base_url = ($problem_name.Replace("_", "-")).Substring(0, ($problem_name.Length) - 2)
 
-# "$problem_name"
-# "$fileDirname"
-# "$folderDirname"
-# "$folderDirname2"
+"$problem_name"
+"$src"
+"$folderBitween"
+"$folderCurrent"
 # "$problem_name_list"
-# "$base_url"
+"$base_url"
 
-if (! (Test-Path $folderDirname2/$problem_name)) {
-    oj dl -d test/$folderDirname2/$problem_name/ https://atcoder.jp/contests/$base_url/tasks/$problem_name
+if (! (Test-Path $folderBitween/$folderCurrent/$problem_name)) {
+    oj dl -d test/$folderBitween/$folderCurrent/$problem_name/ https://atcoder.jp/contests/$base_url/tasks/$problem_name
 }
 
-oj test -c "python $folderDirname/$folderDirname2/$problem_name.py" -d test/$problem_name/
+oj test -c "python $src/$folderBitween/$folderCurrent/$problem_name.py" -d test/$folderBitween/$folderCurrent/$problem_name/
