fb7c4532f40adff4f2304339aebceb8575d3d198
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-25 21:38:45 +0900

    abc235b

--- src/226_250/abc235/abc235_b.py
+++ src/226_250/abc235/abc235_b.py
@@ -1,8 +1,17 @@
 
 # TODO
-# * ABC234 B - Climbing Takahashi
+# * ABC235 B - Climbing Takahashi
 
-n = input()
-h = map(int, input.split())
-print(n)
-print(h)
+n = int(input())
+h = list(map(int, input().split()))
+# print(n)
+# print(h)
+
+ans = h[0]
+# 拡張for文ではなく、range関数で対応
+for i in range(0, n-1):
+    if h[i] < h[i+1]:
+        ans = h[i+1]
+    else:
+        break
+print(ans)
