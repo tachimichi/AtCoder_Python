6739752880be562d17ef62329a9df97b3fc9656f
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-25 21:05:12 +0900

    sacdasd

---
+++ z_python_基礎学習/シリコンバレー/section6_package/talk/animal.py
@@ -0,0 +1,9 @@
+# 絶対参照
+from section6_package.tools import utils
+# 相対参照
+# from ..tools import utils
+
+def sing():
+    return 'bow bow'
+def cry():
+    return utils.say_twice('money')
---
+++ z_python_基礎学習/シリコンバレー/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'python_programming',
+    version = '1.0.0',
+    packages = ['section_package', 'section_package.talk', 'section_package.utils'],
+    url = 'https://atcoder.jp/users/tachimichi',
+    license = 'Free',
+    author = 'ogatat',
+    author_email = 'ogatat',
+    description = 'ogatat',
+)
+
+
