b8e2ef094148afe9dc1e2b87783c759567184a47
tachi_michi@revolve <defxyz714@gmail.com>
2022-01-26 11:01:03 +0900

    235

---
+++ src/226_250/abc232/abc232_a.py
@@ -0,0 +1,6 @@
+# TODO
+# * ABC232 A - QQ solver
+
+x, y = map(int, input().split('x'))
+print(x * y)
+
---
+++ src/226_250/abc232/abc232_b.py
@@ -0,0 +1,20 @@
+# TODO
+# * ABC232 B - Caesar Cipher
+
+s = input()
+t = input()
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+print(s, t, alphabet)
+
+a = []
+count = 0
+
+print(len(s))
+
+
+for i in range(len(s)):
+    print(i)
+
+    si = alphabet.index
