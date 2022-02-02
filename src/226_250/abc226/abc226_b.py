40593b32f2f4dd8781b7a2c7cc309b5024ae937c
tachi_michi@revolve <defxyz714@gmail.com>
2022-02-03 08:54:38 +0900

    226b

--- src/226_250/abc226/abc226_b.py
+++ src/226_250/abc226/abc226_b.py
@@ -1,19 +1,12 @@
 # TODO
-# * ABC228 B - Takahashi's Secret
+# * ABC226 B - Counting Arrays
 
-n, x = map(int, input().split())
-a = list(map(int, input().split()))
+n = int(input())
+# print(n)
 
-box = [0] * n
-# print(box)
-index = x -1
 
-while box[index] == 0:
-    box[index] = 1
-    index = a[index] -1
-
-ans = 0
-for i in range(len(box)):
-    if box[i] == 1:
-        ans += 1
-print(ans)
+d= set()
+for _ in range(n):
+    # print(a)
+    d.add(input())
+print(len(d))
