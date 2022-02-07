da09f4735b95678d4bce424dec0aeff7ead2817c
tachi_michi@revolve <defxyz714@gmail.com>
2022-02-07 19:59:44 +0900

    SQLite

---
+++ z_python_基礎学習/シリコンバレー/section12.py
@@ -0,0 +1,107 @@
+
+print('**************************************')
+#  =================================
+# SQLite
+# 
+'''
+import sqlite3
+
+# comn = sqlite3.connect('test_sqlite.db')
+comn = sqlite3.connect(':memory:')
+
+curs = comn.cursor()
+# curs.execute(
+#     'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+# comn.commit()
+
+# curs.execute(
+#     'INSERT INTO persons(name) values("MIKE")')
+# comn.commit()
+
+# curs.execute(
+#     'INSERT INTO persons(name) values("Nancy")')
+# curs.execute(
+#     'INSERT INTO persons(name) values("Jun")')
+# comn.commit()
+
+# curs.execute('UPDATE persons set name = "Michel" WHERE id = 2')
+# comn.commit()
+
+
+# curs.execute('DELETE FROM persons WHERE name = "Michel"')
+# comn.commit()
+
+curs.execute('SELECT * FROM persons;')
+print(curs.fetchall())
+
+curs.close()
+comn.close()
+# sqlite3 .\test_sqlite.db
+# .tables
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
+print('**************************************')
+#  =================================
+# 
+#
+'''
+'''
